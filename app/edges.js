3c037a68843db1dd30b036e1085a8f2d42e3234e
VaishnavAkash <akashgupta9062@gmail.com>
2024-03-17 18:09:56 +0530

    Update : metadata

---
+++ app/edges.js
@@ -0,0 +1,25 @@
+const initialEdges = [
+    { id: 'akash-2', source: 'poa', target: 'research' },
+    { id: 'akash-3', source: 'poa', target: 'planning' },
+    { id: 'akash-4', source: 'poa', target: 'designing' },
+    { id: 'akash-5', source: 'poa', target: 'manufacturing' },
+    { id: 'akash-6', source: 'poa', target: 'sales/marketing' },
+    { id: 'akash-7', source: 'research', target: 'research1' },
+    { id: 'akash-8', source: 'research', target: 'research2' },
+    { id: 'akash-9', source: 'planning', target: 'planning1' },
+    { id: 'akash-10', source: 'planning', target: 'planning2' },
+    { id: 'akash-11', source: 'designing', target: 'designing1' },
+    { id: 'akash-12', source: 'designing', target: 'designing2' },
+    { id: 'akash-13', source: 'manufacturing', target: 'manufacturing1' },
+    { id: 'akash-14', source: 'manufacturing', target: 'manufacturing2' },
+    { id: 'akash-15', source: 'sales/marketing', target: 'sales/marketing1' },
+    { id: 'akash-16', source: 'sales/marketing', target: 'sales/marketing2' },
+    { id: 'akash-17', source: 'research1', target: 'external1' },
+    { id: 'akash-18', source: 'research1', target: 'external2' },
+    { id: 'akash-19', source: 'external1', target: 'B2B1' },
+    { id: 'akash-20', source: 'external1', target: 'B2B2' },
+    { id: 'akash-21', source: 'external1', target: 'B2B3' },
+    { id: 'akash-22', source: 'external1', target: 'B2B4' },
+];
+
+export default initialEdges;
